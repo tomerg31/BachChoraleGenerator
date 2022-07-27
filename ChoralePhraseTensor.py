93709512fc1a20e5f1cb59bd0801a6170cc49c2b
Tomer Gewirtzman <tomer.gewirtzman@intel.com>
2022-07-27 15:19:22 +0300

    Started working on generator of phrases

--- ChoralePhraseTensor.py
+++ ChoralePhraseTensor.py
@@ -1,23 +1,76 @@
 import torch
 from abstract_chorale_objects import PhraseObject
+from music21.note import Note
+from music21.interval import Interval
 
 MAX_CHORALE_LENGTH = 10
 MAX_PHRASE_LENGTH_IN_QUARTERS = 20
-TONAL_FUNCTIONS = {
+TONAL_FUNCTIONS_TO_INDEX = {
     'TONIC': 0,
     'SUBDOMINANT': 1,
     'DOMINANT': 2
 }
+INDEX_TO_TONAL_FUNCTIONS = {
+    0: 'TONIC',
+    1: 'SUBDOMINANT',
+    2: 'DOMINANT'
+}
+TONAL_FUNCTION_TO_DEGREE = {
+    'TONIC': 0,
+    'SUBDOMINANT': 3,
+    'DOMINANT': 4
+}
+DEGREE_TO_TONAL_FUNCTION = {
+    0: 'TONIC',
+    3: 'SUBDOMINANT',
+    4: 'DOMINANT'
+}
+
 OPENING = 0
 CLOSING = 1
 
 SCALE_DEGREES = 7
+KEY_TO_INT = {
+    'C': 0,
+    'D': 1,
+    'E': 2,
+    'F': 3,
+    'G': 4,
+    'A': 5,
+    'B': 6
+}
+INT_TO_KEY = {
+    0: 'C',
+    1: 'D',
+    2: 'E',
+    3: 'F',
+    4: 'G',
+    5: 'A',
+    6: 'B'
+}
 
 
 def degree_to_one_hot(degree):
     one_hot_degree = torch.zeros(SCALE_DEGREES)
     one_hot_degree[degree - 1] = 1
     return one_hot_degree
+
+
+def scores_to_one_hot(scores: torch.Tensor):
+    one_hot = torch.zeros_like(scores)
+    one_hot[torch.argmax(scores)] = 1
+    return one_hot
+
+
+def one_hot_to_index(one_hot):
+    return int(torch.argmax(one_hot))
+
+
+def degree_from_series_of_relative_degrees(series):
+    output_degree = 0
+    for degree in series:
+        output_degree += degree
+    return output_degree % SCALE_DEGREES
 
 
 class PhraseTensor:
@@ -43,11 +96,11 @@ class PhraseTensor:
         # The opening and closing local tonalities. Since we are working in relative tonalities, we will not care
         # about the absolute pitch of the root. We want the harmonic function in relation to the key of the chorale
         # - meaning either TONIC, DOMINANT, or SUBDOMINANT as a classifier of the local tonality.
-        self.opening_tonality = torch.zeros(len(TONAL_FUNCTIONS))
-        self.opening_tonality[TONAL_FUNCTIONS[self.phrase_object.phrase_tonal_progression[OPENING][0]]] = 1
+        self.opening_tonality = torch.zeros(len(TONAL_FUNCTIONS_TO_INDEX))
+        self.opening_tonality[TONAL_FUNCTIONS_TO_INDEX[self.phrase_object.phrase_tonal_progression[OPENING][0]]] = 1
 
-        self.closing_tonality = torch.zeros(len(TONAL_FUNCTIONS))
-        self.closing_tonality[TONAL_FUNCTIONS[self.phrase_object.phrase_tonal_progression[CLOSING][0]]] = 1
+        self.closing_tonality = torch.zeros(len(TONAL_FUNCTIONS_TO_INDEX))
+        self.closing_tonality[TONAL_FUNCTIONS_TO_INDEX[self.phrase_object.phrase_tonal_progression[CLOSING][0]]] = 1
 
         # We want four core harmonies for each phrase - pickup (if exists), opening downbeat, pre-fermata harmony,
         # and fermata harmony. In addition, we will have the soprano degree. Note that the harmony is represented by
@@ -65,29 +118,29 @@ class PhraseTensor:
         # Downbeat harmony
         self.opening_downbeat_harmony = degree_to_one_hot(
             self.phrase_object.opening_harmony_group.harmony_list[-1].relation_to_local_key
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         self.opening_downbeat_harmony_soprano = degree_to_one_hot(
             self.phrase_object.opening_harmony_group.harmony_list[-1].soprano_degree
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         # Pre-fermata harmony
         self.pre_fermata_harmony = degree_to_one_hot(
             self.phrase_object.closing_harmony_group.harmony_list[-2].relation_to_local_key
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         self.pre_fermata_harmony_soprano = degree_to_one_hot(
             self.phrase_object.closing_harmony_group.harmony_list[-2].soprano_degree
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         # Fermata harmony
         self.fermata_harmony = degree_to_one_hot(
             self.phrase_object.closing_harmony_group.harmony_list[-1].relation_to_local_key
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         self.fermata_harmony_soprano = degree_to_one_hot(
             self.phrase_object.closing_harmony_group.harmony_list[-1].soprano_degree
-        ) if self.phrase_object.pickup else torch.zeros(SCALE_DEGREES)
+        )
 
         # TODO: add inversion to harmonies
 
@@ -108,3 +161,171 @@ class PhraseTensor:
                 self.fermata_harmony_soprano
             ]
         )
+
+
+class GeneratedPhraseTensor:
+    def __init__(self, generator_output):
+        self.generator_output = generator_output
+
+        # generator_output is a 1d tensor which is exactly the shape of a PhraseTensor that came out of a real chorale.
+        # The strategy to translate it back to the features that represent a chorale phrase, is to separate the long
+        # tensor to its parts, and recreate a one hot encoding as in the PhraseTensor object, according to entry with
+        # the highest score in each part
+
+        cut_off_indices = torch.tensor(
+            [
+                MAX_CHORALE_LENGTH,
+                MAX_PHRASE_LENGTH_IN_QUARTERS,
+                len(TONAL_FUNCTIONS_TO_INDEX),
+                len(TONAL_FUNCTIONS_TO_INDEX),
+                SCALE_DEGREES,
+                SCALE_DEGREES,
+                SCALE_DEGREES,
+                SCALE_DEGREES,
+                SCALE_DEGREES,
+                SCALE_DEGREES,
+                SCALE_DEGREES
+            ]
+        )
+        cut_off_indices = torch.cumsum(cut_off_indices, dim=0)
+
+        (
+            index_in_chorale_score,
+            length_in_quarters_score,
+            opening_tonality_score,
+            closing_tonality_score,
+            opening_pickup_harmony_score,
+            opening_pickup_harmony_soprano_score,
+            opening_downbeat_harmony_score,
+            opening_downbeat_harmony_soprano_score,
+            pre_fermata_harmony_score,
+            pre_fermata_harmony_soprano_score,
+            fermata_harmony_score,
+            fermata_harmony_soprano_score
+        ) = torch.tensor_split(self.generator_output, cut_off_indices)
+
+        self.index_in_chorale_one_hot = scores_to_one_hot(index_in_chorale_score)
+        self.length_in_quarters_one_hot = scores_to_one_hot(length_in_quarters_score)
+        self.opening_tonality_one_hot = scores_to_one_hot(opening_tonality_score)
+        self.closing_tonality_one_hot = scores_to_one_hot(closing_tonality_score)
+        self.opening_pickup_harmony_one_hot = scores_to_one_hot(opening_pickup_harmony_score)
+        self.opening_pickup_harmony_soprano_one_hot = scores_to_one_hot(opening_pickup_harmony_soprano_score)
+        self.opening_downbeat_harmony_one_hot = scores_to_one_hot(opening_downbeat_harmony_score)
+        self.opening_downbeat_harmony_soprano_one_hot = scores_to_one_hot(opening_downbeat_harmony_soprano_score)
+        self.pre_fermata_harmony_one_hot = scores_to_one_hot(pre_fermata_harmony_score)
+        self.pre_fermata_harmony_soprano_one_hot = scores_to_one_hot(pre_fermata_harmony_soprano_score)
+        self.fermata_harmony_one_hot = scores_to_one_hot(fermata_harmony_score)
+        self.fermata_harmony_soprano_one_hot = scores_to_one_hot(fermata_harmony_soprano_score)
+
+        # Now that we have the one hot encoding for each feature, we extract the value from each feature
+
+        self.index_in_chorale = one_hot_to_index(self.index_in_chorale_one_hot)
+        self.length_in_quarters = one_hot_to_index(self.length_in_quarters_one_hot) + 1
+        self.opening_tonality_degree = TONAL_FUNCTION_TO_DEGREE[
+            INDEX_TO_TONAL_FUNCTIONS[
+                one_hot_to_index(self.opening_tonality_one_hot)
+            ]
+        ]
+        self.closing_tonality_degree = TONAL_FUNCTION_TO_DEGREE[
+            INDEX_TO_TONAL_FUNCTIONS[
+                one_hot_to_index(self.closing_tonality_one_hot)
+            ]
+        ]
+        # Harmonic degrees are relative to their local tonality
+        self.opening_pickup_harmony_degree = one_hot_to_index(self.opening_pickup_harmony_one_hot)
+        self.opening_downbeat_harmony_degree = one_hot_to_index(self.opening_downbeat_harmony_one_hot)
+        self.pre_fermata_harmony_degree = one_hot_to_index(self.pre_fermata_harmony_one_hot)
+        self.fermata_harmony_degree = one_hot_to_index(self.fermata_harmony_one_hot)
+
+        # Soprano degrees are relative to the harmony root
+        self.opening_pickup_harmony_soprano_degree = one_hot_to_index(self.opening_pickup_harmony_soprano_one_hot)
+        self.opening_downbeat_harmony_soprano_degree = one_hot_to_index(self.opening_downbeat_harmony_soprano_one_hot)
+        self.pre_fermata_harmony_soprano_degree = one_hot_to_index(self.pre_fermata_harmony_soprano_one_hot)
+        self.fermata_harmony_soprano_degree = one_hot_to_index(self.fermata_harmony_soprano_one_hot)
+
+    def display_phrase_information_in_a_given_key(self, key_string='C'):
+        assert key_string in KEY_TO_INT.keys(), "Key must be an uppercase letter [A-G]"
+        key = KEY_TO_INT[key_string]
+        print(f"*****Generated phrase information in the key of {key_string}*****")
+        print(f"Index in chorale = {self.index_in_chorale}")
+        print(f"Length in quarters = {self.length_in_quarters}")
+        print(f"Opening tonality = {DEGREE_TO_TONAL_FUNCTION[self.opening_tonality_degree]}")
+        print(f"Closing tonality = {DEGREE_TO_TONAL_FUNCTION[self.closing_tonality_degree]}")
+        print("Opening pickup harmony = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.opening_tonality_degree,
+                    self.opening_pickup_harmony_degree
+                ])
+            ]
+        ))
+        print("Opening pickup harmony soprano = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.opening_tonality_degree,
+                    self.opening_pickup_harmony_degree,
+                    self.opening_pickup_harmony_soprano_degree
+                ])
+            ]
+        ))
+        print("Opening downbeat harmony = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.opening_tonality_degree,
+                    self.opening_downbeat_harmony_degree
+                ])
+            ]
+        ))
+        print("Opening downbeat harmony soprano = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.opening_tonality_degree,
+                    self.opening_downbeat_harmony_degree,
+                    self.opening_downbeat_harmony_soprano_degree
+                ])
+            ]
+        ))
+        print("Pre-fermata harmony = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.closing_tonality_degree,
+                    self.pre_fermata_harmony_degree
+                ])
+            ]
+        ))
+        print("Pre-fermata harmony soprano = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.closing_tonality_degree,
+                    self.pre_fermata_harmony_degree,
+                    self.pre_fermata_harmony_soprano_degree
+                ])
+            ]
+        ))
+        print("Fermata harmony = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.closing_tonality_degree,
+                    self.fermata_harmony_degree
+                ])
+            ]
+        ))
+        print("Fermata harmony soprano = {0}".format(
+            INT_TO_KEY[
+                degree_from_series_of_relative_degrees([
+                    key,
+                    self.closing_tonality_degree,
+                    self.fermata_harmony_degree,
+                    self.fermata_harmony_soprano_degree
+                ])
+            ]
+        ))
+
+# TODO: Create generator of such tensors.
---
+++ phrase_encoder_decoder.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+from ChoralePhraseTensor import GeneratedPhraseTensor, PhraseTensor
+
+
+class PhraseEncoder(nn.Module):
+    def __init__(self, in_dim, out_dim, number_of_hidden_layers=2):
+        super().__init__()
+
+        modules = []
+
+        layer_sizes = [in_dim]
+        for i in range(number_of_hidden_layers):
+            layer_sizes += [int((layer_sizes[-1] - out_dim) / 2) + out_dim]
+        layer_sizes += [out_dim]
+
+        for in_layer, out_layer in zip(layer_sizes[:-1], layer_sizes[1:]):
+            modules.append(nn.Linear(in_layer, out_layer))
+            modules.append(nn.ReLU())
+
+        # Get rid of that last ReLU
+        self.encoder = nn.Sequential(*modules[:-1])
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class PhraseDecoder(nn.Module):
+    def __init__(self, in_dim, out_dim, number_of_hidden_layers=2):
+        super().__init__()
+
+        modules = []
+
+        # Calculate the layer sizes in reverse order so that it matches the sizes in the encoder
+        layer_sizes = [out_dim]
+        for i in range(number_of_hidden_layers):
+            layer_sizes += [int((layer_sizes[-1] - in_dim) / 2) + in_dim]
+        layer_sizes += [in_dim]
+
+        layer_sizes.reverse()
+
+        for in_layer, out_layer in zip(layer_sizes[:-1], layer_sizes[1:]):
+            modules.append(nn.Linear(in_layer, out_layer))
+            modules.append(nn.ReLU())
+
+        # Get rid of that last ReLU
+        self.decoder = nn.Sequential(*modules[:-1])
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+# This is copied from an exercise from Technion class 236781
+class VAE(nn.Module):
+    def __init__(self, features_encoder, features_decoder, in_size, z_dim):
+        """
+        :param features_encoder: Instance of an encoder that extracts features
+        from an input.
+        :param features_decoder: Instance of a decoder that reconstructs an
+        input from it's features.
+        :param in_size: The size of one input (without batch dimension).
+        :param z_dim: The latent space dimension.
+        """
+        super().__init__()
+        self.features_encoder = features_encoder
+        self.features_decoder = features_decoder
+        self.z_dim = z_dim
+
+        x0 = torch.rand((1, in_size)).to(next(features_encoder.parameters()).device)
+        self.encoder_out_size = features_encoder(x0).shape[1]
+        self.encoder_mean = nn.Linear(self.encoder_out_size, z_dim).to(next(features_encoder.parameters()).device)
+        self.log_sigma2 = nn.Linear(self.encoder_out_size, z_dim).to(next(features_encoder.parameters()).device)
+
+    # def _check_features(self, in_size):
+    #     device = next(self.parameters()).device
+    #     with torch.no_grad():
+    #         # Make sure encoder and decoder are compatible
+    #         x = torch.randn(1, *in_size, device=device)
+    #         h = self.features_encoder(x)
+    #         xr = self.features_decoder(h)
+    #         assert xr.shape == x.shape
+    #         # Return the shape and number of encoded features
+    #         return h.shape[1:], torch.numel(h) // h.shape[0]
+
+    def encode(self, x):
+        out = self.features_encoder(x)
+        mu = self.encoder_mean(out)
+        log_sigma2 = self.log_sigma2(out)
+        random = torch.randn((x.shape[0], self.z_dim)).to(next(self.log_sigma2.parameters()).device)
+        z = torch.sqrt(torch.exp(log_sigma2)) * random + mu
+
+        return z, mu, log_sigma2
+
+    def decode(self, z):
+        return self.features_decoder(z)
+
+    def sample(self, n):
+        with torch.no_grad():
+            z = torch.randn((n, self.z_dim)).to(next(self.log_sigma2.parameters()).device)
+            z = self.z_to_h(z).unsqueeze(2).unsqueeze(3)
+            samples = self.features_decoder(z)
+
+        samples = [s.detach().cpu() for s in samples]
+        return samples
+
+    def forward(self, x):
+        z, mu, log_sigma2 = self.encode(x)
+        return self.decode(z), mu, log_sigma2
+
+
+def vae_loss(x, xr, z_mu, z_log_sigma2, x_sigma2):
+    """
+    Point-wise loss function of a VAE with latent space of dimension z_dim.
+    :param x: Input image batch of shape (N,C,H,W).
+    :param xr: Reconstructed (output) image batch.
+    :param z_mu: Posterior mean (batch) of shape (N, z_dim).
+    :param z_log_sigma2: Posterior log-variance (batch) of shape (N, z_dim).
+    :param x_sigma2: Likelihood variance (scalar).
+    :return:
+        - The VAE loss
+        - The data loss term
+        - The KL divergence loss term
+    all three are scalars, averaged over the batch dimension.
+    """
+    reg = 1 / (x_sigma2 * x.shape[1] * x.shape[2] * x.shape[3])
+    data_loss = reg * torch.norm((x - xr).reshape((x.shape[0], -1)), dim=1) ** 2.0
+    kldiv_loss = (
+        torch.sum(torch.exp(z_log_sigma2), dim=1) +
+        torch.norm(z_mu, dim=1) ** 2.0 - z_mu.shape[1] -
+        torch.sum(z_log_sigma2, dim=1)
+    )
+
+    data_loss = torch.mean(data_loss)
+    kldiv_loss = torch.mean(kldiv_loss)
+    loss = data_loss + kldiv_loss
+
+    return loss, data_loss, kldiv_loss
